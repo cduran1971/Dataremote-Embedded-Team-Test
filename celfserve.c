d46d99ac452b45338beb7cea08299f8a405009b9
Celso Duran <53011778+cduran1971@users.noreply.github.com>
2023-02-23 19:48:53 -0500

    Started working on send buffer
    Todo: Make buffer efficient and implement send to client

--- celfserve.c
+++ celfserve.c
@@ -8,39 +8,118 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 const unsigned short kMAX_PORT_NUMBER = 65535;
+const char kINET_ADDR[] = "127.0.0.1";
+const int kMAX_BIND_RETRIES = 3;
+const int kinput_buffer_SIZE = 1024;
+
+void errout(char *err_string) 
+{
+	printf("%s\n", err_string);
+	exit(EXIT_FAILURE);
+}
 
 // Make sure the argument is a valid port number
-_Bool is_port_number(char *a_string) {
+int is_port_number(char *a_string)
+{
 	int length = strlen(a_string);
 
 	// Port numbers have at most 5 digits
 	if(length > 6)
-		return false;
+		return 0;
 
 	for(int i=0; i < length; ++i) {
 		if(a_string[i] < '0' || a_string[i] > '9' )
-			return false;
+			return 0;
 	}
 
 	if(atoi(a_string) > kMAX_PORT_NUMBER)
-		return false;
+		return 0;
 
-	return true;
+	return 1;
 }
 
-// If a port number of 0 is given the server will bind to the
-// first open port and display the port used
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	unsigned short port_num = 0;
-	
-	if((argc != 2) || (!is_port_number(argv[1]))) {
-		printf("Invalid argument(s)\n");
-		exit(EXIT_FAILURE);
+	int bindTries = kMAX_BIND_RETRIES;
+	int socketId;
+  	struct sockaddr_in my_sock, their_sock;
+  	char input_buffer[kinput_buffer_SIZE];
+  	socklen_t my_addr_size, their_addr_size;
+
+  	// ----------------------------------
+  	char output_buffer[10240];
+  	int output_buffer_index = 0;
+  	int input_buffer_index = 0;
+  	int num_chars_in;
+  	// ----------------------------------
+
+	if((argc != 2) || (!is_port_number(argv[1])))
+	{
+		errout("Invalid argument(s)");
 	}
 
+	if(!(socketId = socket(AF_INET, SOCK_DGRAM, 0)))
+	{
+		errout("Socket error");
+	}
+	
 	port_num = atoi(argv[1]);
-	printf("Celfserve listening on port: %d\n", atoi(argv[1]));
+
+	my_addr_size = sizeof(my_sock);
+	memset(&my_sock, '\0', my_addr_size);
+ 	my_sock.sin_family = AF_INET;
+ 	my_sock.sin_port = htons(port_num);
+ 	my_sock.sin_addr.s_addr = inet_addr(kINET_ADDR);
+
+ 	while(bindTries--)
+ 	{
+ 		if(bind(socketId, (struct sockaddr*)&my_sock, my_addr_size) == 0)
+ 		{
+ 			getsockname(socketId, (struct sockaddr*)&my_sock, &my_addr_size);
+ 			port_num = ntohs(my_sock.sin_port);
+ 			printf("Celfserve listening on port: %d\n", port_num);
+ 			break;
+ 		}
+
+ 		if(port_num == 0)
+ 		{
+ 			errout("Bind error occured");
+ 		} else {
+ 			printf("Port %d cannot be used, searching for next available port...\n", port_num);
+ 			port_num = 0;
+ 			my_sock.sin_port = htons(port_num);
+ 		}
+ 	}
+
+ 	their_addr_size = sizeof(their_sock);
+
+ 	while(1) {
+ 		if ((num_chars_in = recvfrom(socketId, input_buffer, sizeof(input_buffer), 0, (struct sockaddr*)&their_sock, &their_addr_size)) < 0)
+ 		{
+        	errout("Receive error occured");
+    	}
+
+    	for(input_buffer_index=0; input_buffer_index < num_chars_in; input_buffer_index++) {
+    		output_buffer[output_buffer_index] = input_buffer[input_buffer_index];
+    		output_buffer_index++;
+    	}
+
+    	if(strchr(output_buffer, (char)13))
+    	{
+    		output_buffer[output_buffer_index] = 0;
+			printf("%s\n", output_buffer);
+			output_buffer_index = 0;	// Clear the output buffer
+    	}
+ 	}
+ 	
+ 	return 0;
 }
+
